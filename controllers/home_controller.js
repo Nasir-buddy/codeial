99891f2187677b8e76295f3285b75dab38d05a07
Nasir-Buddy <syedali172000@gmail.com>
2023-11-01 13:07:13 +0530

    created post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,11 @@
-module.exports.home = (req, res)=>{
-    console.log("Cookies value : ", req.cookies);
-    return res.render('home',{
-        title: "Home"
+const Post = require('../models/post');
+module.exports.home = async (req, res) => {
+    // console.log("Cookies value : ", req.cookies);
+    const posts = await Post.find({});
+
+    // Render the home page with the posts
+    res.render('home', {
+        title: 'Codeial | Home',
+        posts: posts,
     });
-}  
+}
---
+++ controllers/posts_controller.js
@@ -0,0 +1,13 @@
+const Post = require('../models/post');
+module.exports.create = async (req, res) => {
+  try {
+      const post = await Post.create({
+          content: req.body.content,
+          user: req.user._id
+      });
+      return res.redirect('back');
+  } catch (err) {
+      console.log('Error in creating a post: ', err);
+      return res.status(500).json({ error: 'Failed to create post' });
+  }
+};
