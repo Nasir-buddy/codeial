e271fc3575d7b2252d1841b2e14bd4a75cd1140c
Nasir-Buddy <syedali172000@gmail.com>
2023-11-23 12:48:04 +0530

    [Codeial]: implemented flash message

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,48 +1,48 @@
 const User = require('../models/user')
 
-module.exports.profile = async (req, res)=>{
+module.exports.profile = async (req, res) => {
     // console.log('req user' ,req.user);
-    const user = await User.findById(req.params.id, function(err, user){
-        return res.render('users_profile',{
+    const user = await User.findById(req.params.id, function (err, user) {
+        return res.render('users_profile', {
             title: "Entered in users directory",
-            profile_user : user
+            profile_user: user
         });
     });
     // console.log("user controller ",req.user.name)  
 }
 
 //render the sign up page 
-module.exports.signUp = function(req, res){
-    if(req.isAuthenticated()){
+module.exports.signUp = function (req, res) {
+    if (req.isAuthenticated()) {
         return res.redirect('/users/profile');
     }
-    return res.render('user_sign_up',{
+    return res.render('user_sign_up', {
         title: "Codeial | Sign Up"
     })
 }
 
 // render the sign in page
-module.exports.singIn = function(req, res){
-    if(req.isAuthenticated()){
+module.exports.singIn = function (req, res) {
+    if (req.isAuthenticated()) {
         return res.redirect('/users/profile')
     }
-    return res.render('user_sign_in',{
+    return res.render('user_sign_in', {
         title: "Codeial | Sign In"
     })
 }
 
 //get the sign up data
-module.exports.create = async (req, res)=>{
+module.exports.create = async (req, res) => {
     console.log("enter singup")
-    if(req.body.password !== req.body.confirm_password){
+    if (req.body.password !== req.body.confirm_password) {
         return res.redirect('/users/sign-in');
     }
     console.log("exit route")
-   
-    try{
-        const existingUser = await User.findOne({email: req.body.email});
+
+    try {
+        const existingUser = await User.findOne({ email: req.body.email });
         console.log(existingUser)
-        if(!existingUser){
+        if (!existingUser) {
             console.log("create user")
             await User.create(req.body);
             return res.redirect('/users/sign-in');
@@ -57,12 +57,14 @@ module.exports.create = async (req, res)=>{
 
 
 // sign in and get the create session 
-module.exports.createSession = function(req, res){
+module.exports.createSession = function (req, res) {
+    req.flash('success', 'Logged in Successfully');
     return res.redirect('/');
 }
 
 module.exports.destroySession = (req, res) => {
-    req.logout(function(err) {
+    req.logout(function (err) {
+        req.flash('success', 'You have logged out');
         if (err) {
             // Handle the error
             return;
--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
+const flash = require('connect-flash');
+const customMware = require('./config/middleware');
 // const sassMiddleware = require('node-sass-middleware');
 // app.use(sassMiddleware({
 //     src: './assets/scss',
@@ -60,6 +62,9 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(passport.setAuthenticatedUser);
+
+app.use(flash());
+app.use(customMware.setFlash);
 // use express router
 app.use('/', require('./routes'));
 
